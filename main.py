706641e342cf9821518c4fd060bf771eea0ae318
jonthesquirrel <jdh11235@gmail.com>
2020-12-31 02:52:30 -0600

    List average color of every block texture

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 from numpy import interp
+from os import listdir
 from PIL import Image, ImageStat
 
-# Open Minecraft texture as RGBA image
-im = Image.open('assets/minecraft/textures/block/acacia_log.png')
+# Block textures directory extracted from version jar
+textures = 'assets/minecraft/textures/block'
 
-# Convert RGBA image into HSV (Hue, Saturation, Value) image
-im = im.convert('HSV')
+# Find png files in textures directory
+files = [file for file in listdir(textures) if file.endswith('.png')]
 
-# Split HSV into separate channels
-hue_channel = im.getchannel('H')
-sat_channel = im.getchannel('S')
-val_channel = im.getchannel('V')
+for file in files:
+    # Open Minecraft texture as RGBA image
+    im = Image.open(f'{textures}/{file}')
 
-# Get average of each channel
-h = ImageStat.Stat(hue_channel).mean
-s = ImageStat.Stat(sat_channel).mean
-v = ImageStat.Stat(val_channel).mean
+    # Convert RGBA image into HSV (Hue, Saturation, Value) image
+    im = im.convert('HSV')
 
-# Scale from 8-bit channel (255, 255, 255) to hsv(360°, 100%, 100%) range
-h = interp(h, [0, 255], [0, 360])[0]
-s = interp(s, [0, 255], [0, 100])[0]
-v = interp(v, [0, 255], [0, 100])[0]
+    # Split HSV into separate channels
+    hue_channel = im.getchannel('H')
+    sat_channel = im.getchannel('S')
+    val_channel = im.getchannel('V')
 
-# Print average HSV of image as hsv() color code
-hsv_string = f'hsv({round(h)}, {round(s)}%, {round(v)}%)'
-print (hsv_string)
+    # Get average of each channel
+    h = ImageStat.Stat(hue_channel).mean
+    s = ImageStat.Stat(sat_channel).mean
+    v = ImageStat.Stat(val_channel).mean
+
+    # Scale from 8-bit channel (255, 255, 255) to hsv(360°, 100%, 100%) range
+    h = interp(h, [0, 255], [0, 360])[0]
+    s = interp(s, [0, 255], [0, 100])[0]
+    v = interp(v, [0, 255], [0, 100])[0]
+
+    # Print average HSV of image as hsv() color code
+    hsv_string = f'hsv({round(h)}, {round(s)}%, {round(v)}%)'
+    print (f'{file} : {hsv_string}')
