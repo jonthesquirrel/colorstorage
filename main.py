70c575f9d9d916ea23fffa3c9e12d19dcf9210cb
jonthesquirrel <jdh11235@gmail.com>
2020-12-31 01:23:48 -0600

    Add comments to explain code

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from numpy import interp
 from PIL import Image, ImageColor, ImageStat
 
-# print average HSV of image
-
+# Open Minecraft texture as RGBA image
 im = Image.open('assets/minecraft/textures/block/acacia_log.png')
 
+# Convert RGBA image into HSV (Hue, Saturation, Value) image
 im = im.convert('HSV')
+
+# Split HSV into separate channels
 hue_channel = im.getchannel('H')
 sat_channel = im.getchannel('S')
 val_channel = im.getchannel('V')
+
+# Get average of each channel
 h = ImageStat.Stat(hue_channel).mean
 s = ImageStat.Stat(sat_channel).mean
 v = ImageStat.Stat(val_channel).mean
+
+# Scale from 8-bit channel (255, 255, 255) to hsv(360°, 100%, 100%) range
 h = interp(h, [0, 255], [0, 360])[0]
 s = interp(s, [0, 255], [0, 100])[0]
 v = interp(v, [0, 255], [0, 100])[0]
 
+# Print average HSV of image as hsv() code
 hsv_string = f'hsv({round(h)}, {round(s)}%, {round(v)}%)'
 print (hsv_string)
